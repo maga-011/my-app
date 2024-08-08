94e9de318498f8dc0f81a4c4b63ad6a4526b4b96
muhamed <muhamedkarabagli08@gmail.com>
2024-08-08 17:22:13 +0400

    first commit

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import styles from './register.module.css'
+
+
+const Register = () => {
+  return <div className={styles.container}> 
+  <div className={styles.contentWrapper}>
+<form action="" className={styles.form}>
+<input type="text" placeholder='username' />
+<input type="email" placeholder='email' />
+<input type="password " placeholder='password'/>
+<input type="password" placeholder='password again' />
+<button> register </button>
+<Link href={'./login'}> have an account? <b>Login</b> </Link>
+</form>
+  </div>
+  
+  </div>;
+};
+
+export default Register;
